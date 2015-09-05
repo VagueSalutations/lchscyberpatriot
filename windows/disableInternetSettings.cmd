b728718983229204d9978a57df0b8ffd0f092d9a
geoffreyfierro <geoffreyfierro@gmail.com>
2015-09-05 15:43:00 -0700

    Update disableInternetSettings.cmd

--- windows/disableInternetSettings.cmd
+++ windows/disableInternetSettings.cmd
@@ -2,5 +2,5 @@
 :: NEEDS TO BE TESTED
 
 FOR /F "delims==" %%G IN (disableServices.txt) DO net stop %%G
-FOR /F "delims==" %%G IN (disableServices.txt) DO sc config %%G start= disabled
+FOR /F "delims==" %%G IN (disableServices.txt) DO sc config %%G start= disabled ::not working
 :: create disableServices.txt file, include TELNET and "MICROSOFT FTP SERVICE"
