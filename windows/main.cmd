9a357c53745895c2db218792c8870a16fa7d57d5
Sunspark <VagueSalutations@users.noreply.github.com>
2015-09-05 17:02:30 -0700

    Update main.cmd

--- windows/main.cmd
+++ windows/main.cmd
@@ -1,7 +1,8 @@
 
-:: Create badids.txt *FIRST*
-:: Create disableServices.txt *FIRST*
+:: Create badids.txt *FIRST* (if no bad ids, create anyway)
+:: Create disableServices.txt *FIRST* (NET START, list services to terminate)
+:: Create usernames.txt *FIRST* (lists all usernames)
 
 deleteBadIDs.cmd
-manageAccounts.cmd              :: auto-calls mangleNetUser.cmd
+manageAccounts.cmd              :: auto-calls mangleNetUser.cmd (dependency on usernames.txt)
 disableInternetSettings.cmd
